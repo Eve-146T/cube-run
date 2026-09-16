615bbae414b275b315b92e20815ee4d0c01888c1
Eve-146T <Eve-146T@users.noreply.github.com>
2026-09-16 09:13:13 +0200

    Prepare Cube Run 2.4 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "cube.run"
         minSdk = 28
         targetSdk = 35
-        versionCode = 10
-        versionName = "2.3"
+        versionCode = 11
+        versionName = "2.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
